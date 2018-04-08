f73e3ba7eb7ee3e058fdca06741ed5f0564a4da4
fionanealon <37928647+fionanealon@users.noreply.github.com>
2018-04-08 17:55:36 +0100

    Add files via upload

--- euler5.py
+++ euler5.py
@@ -1,17 +1,23 @@
-# Fiona Neealon, 04-05-2018
-# "The smallest positive number that is evenly divisible by all of the numbers from 1 to 20"
-
+# Fiona Neealon, 2018-04-2018
+# A program that finds the smallest positive number that is evenly divisible by all of the numbers from 1 to 20
+# Adapted from: https://stackoverflow.com/questions/8024911/project-euler-5-in-python-how-can-i-optimize-my-solution
 
 def factorial(upto):
+# Create a variable that will become the answer
   multupto = 1
+# Loop through numbers i from 1 to upto
   for i in range (1, upto):
+# If the number is not divisible by i
     if multupto % i > 0: 
+# Loop through numbers k from 1 to upto
         for k in range (1, upto):
-            if (multupto * k) % i == 0: 
+# Find the smallest number divisible by i   
+            if (multupto * k) % i == 0:
+# Multiply ans by k, changing ans to that
                 multupto = multupto * k
                 break
   return multupto
 
-print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20", factorial(4)) 
+print("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20", factorial(20)) 
   
 
